c89e0875e94d4f4abde74f07c442900953ff2bfe
iammessier <nahatajay6@gmail.com>
2023-10-26 15:29:20 +0530

    refactor

---
+++ refactoring.py
@@ -0,0 +1,7 @@
+#Refactoring below code by using pylance in vscode.
+yy = 2017
+print(calendar.calendar(yy))
+cwd = os.getcwd()
+print("Current working directory:", cwd)
+result = os.path.exists("file_name")
+print(result)
